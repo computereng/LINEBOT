5e6f6a7b6ca1d47f74de7909ac1da3a55fdba191
computereng <worawuth.kab@siam.edu>
2017-03-09 12:04:07 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -15,14 +15,13 @@ if (!is_null($events['events'])) {
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
+			
 
 			// Build message to reply back
-			if ($text == "hello")
-			{
+			if ($event['text'] == 'Hello')
 			$messages = [
 				'type' => 'text',
-				'text' => "I AM Proxima"
-			}
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
