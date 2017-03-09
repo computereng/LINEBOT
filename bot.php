8387e7d931afee50e506642b668f8bb15008e48f
computereng <worawuth.kab@siam.edu>
2017-03-09 12:05:38 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -18,7 +18,7 @@ if (!is_null($events['events'])) {
 			
 
 			// Build message to reply back
-			if ($event['text'] == 'Hello')
+			if ($event['message'] == 'Hello')
 			$messages = [
 				'type' => 'text',
 				'text' => $text
