9fd7d18bdd3789788137f201a5eb42d8cdd96f29
computereng <worawuth.kab@siam.edu>
2017-03-13 01:25:10 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -52,7 +52,7 @@ if (!is_null($events['events'])) {
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 			$collection->insert($data)
-			$ch = curl_init($url);
+			//$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
