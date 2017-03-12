96cd80908eecc5df656b8582ce260f2e4aed8a97
computereng <worawuth.kab@siam.edu>
2017-03-13 01:09:13 +0700

    Update Testmongo.php

--- Testmongo.php
+++ Testmongo.php
@@ -1,41 +1,32 @@
 <?php
 
 $m = new MongoClient();
-$collection = $m->selectCollection('test', 'phpmanual');
-
-// If an array literal is used, there is no way to access the generated _id
-$collection->insert(array('x' => 1));
-
-// The _id is available on an array passed by value
-$a = array('x' => 2);
-$collection->insert($a);
-var_dump($a);
-
-// The _id is not available on an array passed by reference
-$b = array('x' => 3);
-$ref = &$b;
-$collection->insert($ref);
-var_dump($ref);
-
-// The _id is available if a wrapping function does not trigger copy-on-write
-function insert_no_cow($collection, $document)
-{
-    $collection->insert($document);
-}
-
-$c = array('x' => 4);
-insert_no_cow($collection, $c);
-var_dump($c);
-
-// The _id is not available if a wrapping function triggers copy-on-write
-function insert_cow($collection, $document)
-{
-    $document['y'] = 1;
-    $collection->insert($document);
-}
-
-$d = array('x' => 5);
-insert_cow($collection, $d);
-var_dump($d);
+        $db = $m->test;
+       
+        $inset = "db.getCollection('foo').insert({'name':'nanhe','age':30});";
+        $response = $db->execute($inset);
+        print_r($response); //Array ( [retval] => [ok] => 1 ) 
+       
+        $response = $m->test->execute("db.getCollection('foo').insert({'name':'happy','age':18});");
+        print_r($response); //Array ( [retval] => [ok] => 1 ) 
+        
+        $response = $m->test->execute("db.foo.insert({'name':'prince','age':16});");
+        print_r($response); //Array ( [retval] => [ok] => 1 ) 
+        
+        $response= $m->test->execute("return db.foo.count();"); 
+        print_r($response); //Array ( [retval] => 3 [ok] => 1 ) 
+        
+        $response= $m->test->execute("return db.foo.findOne();"); 
+        print_r($response); //Array ( [retval] => Array ( [_id] => MongoId Object ( [$id] => 5287ccbe60e2eac9a0e2f1c6 ) [name] => nanhe [age] => 30 ) [ok] => 1 ) 
+        
+        /*
+         * If you want use find function then use toArray because The find() function returns a cursor, which can't be returned from JavaScript.
+         */
+        $response= $m->test->execute("return db.foo.find().toArray();"); 
+        print_r($response); //[$id] => 5287cd2260e2eac9a0e2f1ca ) [name] => happy [age] => 18 ) [2] => Array ( [_id] => MongoId Object ( [$id] => 5287cd2260e2eac9a0e2f1cb ) [name] => prince [age] => 16 ) [3] => Array ( [_id] => MongoId Object ( [$id] => 5287cdea60e2eac9a0e2f1cc ) [name] => nanhe [age] => 30 ) [4] => Array ( [_id] => MongoId Object ( [$id] => 5287cdea60e2eac9a0e2f1cd ) [name] => happy [age] => 18 ) [5] => Array ( [_id] => MongoId Object ( [$id] => 5287cdea60e2eac9a0e2f1ce ) [name] => prince [age] => 16 ) ) [ok] => 1 ) 
+       
+        $response= $m->test->execute("return db.foo.find({'name':'nanhe'}).toArray();"); 
+        print_r($response); //Array ( [retval] => Array ( [0] => Array ( [_id] => MongoId Object ( [$id] => 5287ce9b60e2eac9a0e2f1d2 ) [name] => nanhe [age] => 30 ) ) [ok] => 1 ) 
+        // $id value will be different in your case
 
 ?>
